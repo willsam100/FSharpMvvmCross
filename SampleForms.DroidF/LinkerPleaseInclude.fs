246137f9adfbd98e8f1bc3a81c7831ebbe6e1a1f
Sam Williams <willsam100@gmail.com>
2017-02-11 20:21:29 +1300

    Update conversion to F# for droid

--- SampleForms.DroidF/LinkerPleaseInclude.fs
+++ SampleForms.DroidF/LinkerPleaseInclude.fs
@@ -1,1 +1,83 @@
-﻿
+﻿namespace SampleForms.DroidF
+
+open System
+open System.Collections.Specialized
+open System.Windows.Input
+open Android.App
+open Android.Views
+open Android.Widget
+
+//#pragma warning disable 219
+
+
+// This class is never actually executed, but when Xamarin linking is enabled it does how to ensure types and properties
+// are preserved in the deployed app
+type LinkerPleaseInclude() = 
+
+    member this.Include(button: Button) = 
+        button.Click.AddHandler <| EventHandler(fun s e -> button.Text <- button.Text + "")
+    
+    member this.Include(checkBox: CheckBox) = 
+        EventHandler<CompoundButton.CheckedChangeEventArgs>(fun sender args -> checkBox.Checked <- not checkBox.Checked) 
+        |> checkBox.CheckedChange.AddHandler
+    
+    member this.Include(``switch``: Switch) = 
+        EventHandler<CompoundButton.CheckedChangeEventArgs>(fun sender args -> ``switch``.Checked <- not ``switch``.Checked)
+        |> ``switch``.CheckedChange.AddHandler
+    
+    member this.Include(view: View) = 
+        EventHandler(fun s e  -> view.ContentDescription <- view.ContentDescription + "")
+        |> view.Click.AddHandler
+    
+    member this.Include(text: TextView) = 
+    
+        text.TextChanged += (sender, args) => text.Text = "" + text.Text
+    //    text.Hint = "" + text.Hint
+    
+
+    //member this.Include(CheckedTextView text)
+    
+    //    text.TextChanged += (sender, args) => text.Text = "" + text.Text
+    //    text.Hint = "" + text.Hint
+    
+
+    //member this.Include(CompoundButton cb)
+    
+    //    cb.CheckedChange += (sender, args) => cb.Checked = !cb.Checked
+    
+
+    //member this.Include(SeekBar sb)
+    
+    //    sb.ProgressChanged += (sender, args) => sb.Progress = sb.Progress + 1
+    
+
+    //member this.Include(Activity act)
+    
+    //    act.Title = act.Title + ""
+    
+
+    //member this.Include(INotifyCollectionChanged changed)
+    
+    //    changed.CollectionChanged += (s, e) =>  var test = string.Format("01234", e.Action, e.NewItems, e.NewStartingIndex, e.OldItems, e.OldStartingIndex) 
+    
+
+    //member this.Include(ICommand command)
+    
+    //    command.CanExecuteChanged += (s, e) =>  if (command.CanExecute(null)) command.Execute(null) 
+    
+
+    //member this.Include(MvvmCross.Platform.IoC.MvxPropertyInjector injector)
+    
+    //    injector = new MvvmCross.Platform.IoC.MvxPropertyInjector()
+    
+
+    //member this.Include(System.ComponentModel.INotifyPropertyChanged changed)
+    
+    //    changed.PropertyChanged += (sender, e) =>
+        
+    //        var test = e.PropertyName
+        
+        
+    
+
+
--- SampleForms.DroidF/Main.fs
+++ SampleForms.DroidF/Main.fs
@@ -1,10 +1,10 @@
 ﻿namespace SampleForms.DroidF
 
 open System
+open System.Diagnostics
 open System.Collections.Generic
 open System.Linq
 open System.Text
-open
 open Android.App
 open Android.Content
 open Android.OS
@@ -19,6 +19,39 @@ open MvvmCross.Platform
 open MvvmCross.Core.Views
 open MvvmCross.Forms.Presenter.Droid
 open MvvmCross.Core.ViewModels
+open MvvmCross.Platform.Platform
+open MvvmCross.Droid.Platform
+open MvvmCross.Droid.Views
+
+type DebugTrace() as this = 
+    interface IMvxTrace with
+        member this.Trace(level: MvxTraceLevel , tag: string , message: Func<string>) = 
+            Debug.WriteLine <| sprintf "%s:%A:%s" tag level (message.Invoke())
+        
+        member this.Trace(level: MvxTraceLevel , tag: string , message: string) =
+            Debug.WriteLine <| sprintf "%s:%A:%s" tag level message
+        
+
+        member this.Trace(level: MvxTraceLevel , tag: string , message: string , [<ParamArray>] args: Object[]) = 
+            let rec trace level tag message (args: Object[]) = 
+                try
+                    Debug.WriteLine <| sprintf "%s:%A:%s, %A" tag level message args
+                with 
+                | e as FormatException -> 
+                    trace MvxTraceLevel.Error tag "Exception during trace of 0 1" [|level; message|]
+            trace level tag message args
+    
+type Setup(applicationContext: Context) =
+    inherit MvxAndroidSetup(applicationContext)
+
+    override this.CreateApp() : IMvxApplication = new Core.App()
+
+    override this.CreateDebugTrace(): IMvxTrace = new DebugTrace() :> IMvxTrace
+
+    override this. CreateViewPresenter() : IMvxAndroidViewPresenter = 
+        let presenter = new MvxFormsDroidPagePresenter()
+        Mvx.RegisterSingleton<IMvxViewPresenter>(presenter)
+        presenter :> IMvxAndroidViewPresenter
 
 
 type Resources = SampleForms.DroidF.Resource
